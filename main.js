1e3e8470abf2e9f9e7e9189d7f64adab6b5f981a
mariagomespassos <maria.gomes.passos@escola.pr.gov.br>
2024-09-26 12:04:41 -0300

    .

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.write (quanto);
 function soma (non1, non2){
     return non1+non2;
 }
-function areaquadrado (lado1,lado2){
+function areaquadrado (quanto){
     return quanto**2;
 }
 console.log(soma(1,3));
