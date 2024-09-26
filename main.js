7d00d922e7dad94890f07bb5b08b5c7cae6da792
mariagomespassos <maria.gomes.passos@escola.pr.gov.br>
2024-09-26 12:28:31 -0300

    .

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 alert('algo');
 let quanto = parseInt(prompt('insira um número igual'));
-document.write (quanto);
+document.write (quanto1);
+let quanto = parseInt(prompt('insira um número igual'));
+document.write (quanto2);
+let quanto = parseInt(prompt('insira um número igual'));
+document.write (quanto3);
 
 function soma (non1, non2){
     return non1+non2;
 }
 function areaquadrado (quanto){
     return quanto**2;
-}
+    let media = function(quanto1, quanto2, quanto3)
+};
+document(quanto1, quanto2, quanto3);
 console.log(soma(1, 3);
-console.log( areaQ (quanto))
+console.log( areaQ (quanto1));
+console.log(media(quanto1, quanto2, quanto3));
