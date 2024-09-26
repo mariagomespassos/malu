0b262d850b9a4481eba01504cb8a4d800591a352
mariagomespassos <maria.gomes.passos@escola.pr.gov.br>
2024-09-26 11:55:58 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+alert('algo')
 let quanto = parseInt(prompt('insira 2 números iguais '))
+document write (quanto);
 function soma (non1, non2){
     return non1+non2;
 }
@@ -7,4 +9,3 @@ function areaquadrado (lado1,lado2){
 }
 console.log(soma(1,3));
 console.log(areaquadrado(80,80));
-document write (quanto);
